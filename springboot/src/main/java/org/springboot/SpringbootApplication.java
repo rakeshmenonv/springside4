339b80828d3ae76aa56356c3786a3b723ed5ec20
rakesh menon <rakeshmenon.v@gmail.com>
2015-03-18 14:34:04 +0800

    bug fix

--- springboot/src/main/java/org/springboot/SpringbootApplication.java
+++ springboot/src/main/java/org/springboot/SpringbootApplication.java
@@ -6,31 +6,19 @@ import org.springboot.config.WebAppInitializer;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@Configuration
-@ComponentScan({"org.springboot"})
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableJpaRepositories
 @Import({WebAppInitializer.class})
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
-    	
-//    	ApplicationContext ctx = (ApplicationContext) SpringApplication.run(SpringbootApplication.class,
-//				args);
-//
-//		System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//		String[] beanNames = ((ListableBeanFactory) ctx).getBeanDefinitionNames();
-//		Arrays.sort(beanNames);
-//		for (String beanName : beanNames) {
-//			System.out.println(beanName);
-//		}
+
     }
 }
--- springboot/src/main/java/org/springboot/config/WebAppInitializer.java
+++ springboot/src/main/java/org/springboot/config/WebAppInitializer.java
@@ -12,7 +12,9 @@ import javax.servlet.ServletRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springboot.common.StaticContentServlet;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -28,96 +30,24 @@ import org.springframework.web.servlet.DispatcherServlet;
  * @author <a href="http://rockhoppertech.com/blog/">Gene De Lisa</a>
  * 
  */
-@Configuration
+@SpringBootApplication
+@ImportResource({"classpath:applicationContext-shiro.xml"})
 public class WebAppInitializer implements WebApplicationInitializer {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(WebAppInitializer.class);
 
-	/**
-	 * 
-	 * 
-	 * @see org.springframework.web.WebApplicationInitializer#onStartup(javax.servlet
-	 *      .ServletContext)
-	 */
 	@Override
 	public void onStartup(ServletContext servletContext)
 			throws ServletException {
-
-		// Create the root appcontext
-		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-		rootContext.register(RootConfig.class);
-
-		// if you're not passing in the config classes to the constructor,
-		// refresh
-		rootContext.refresh();
-
-		// Manage the lifecycle of the root appcontext
-		servletContext.addListener(new ContextLoaderListener(rootContext));
-		servletContext.setInitParameter("defaultHtmlEscape", "true");
-		OpenEntityManagerInViewFilter  openEntityManagerInViewFilter = new OpenEntityManagerInViewFilter();
-		servletContext.addFilter("openEntityManagerInViewFilter", openEntityManagerInViewFilter).addMappingForServletNames(null, false, "/*");
-	
-		
 		DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
 		delegatingFilterProxy.setTargetFilterLifecycle(true);
 		EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(
                 DispatcherType.REQUEST, DispatcherType.FORWARD,DispatcherType.INCLUDE,DispatcherType.ERROR);
 		servletContext.addFilter("shiroFilter", delegatingFilterProxy).addMappingForServletNames(dispatcherTypes, false, "/*");
-		servletContext.addServlet("StaticContentServlet",new StaticContentServlet()).addMapping("/download");
 		servletContext.addListener(new SessionListener());
-		// now the config for the Dispatcher servlet
-		AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
-		mvcContext.register(WebMvcConfig.class);
 
-		// Filters
-		// http://static.springsource.org/spring/docs/3.1.x/javadoc-api/org/springframework/web/filter/package-summary.html
-
-		// Enables support for DELETE and PUT request methods with web browser
-		// clients
-		// http://static.springsource.org/spring/docs/3.1.x/javadoc-api/org/springframework/web/filter/HiddenHttpMethodFilter.html
-
-		FilterRegistration.Dynamic fr = servletContext.addFilter(
-				"hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
-		fr.addMappingForUrlPatterns(null, true, "/*");
-
-		fr = servletContext.addFilter("encodingFilter",
-				new CharacterEncodingFilter());
-		fr.setInitParameter("encoding", "UTF-8");
-		fr.setInitParameter("forceEncoding", "true");
-		fr.addMappingForUrlPatterns(null, true, "/*");
-
-		// The main Spring MVC servlet.
-		ServletRegistration.Dynamic appServlet = servletContext.addServlet(
-				"appServlet", new DispatcherServlet(mvcContext));
-		appServlet.setLoadOnStartup(1);
-		Set<String> mappingConflicts = appServlet.addMapping("/");
-
-		if (!mappingConflicts.isEmpty()) {
-			for (String s : mappingConflicts) {
-				logger.error("Mapping conflict: " + s);
-			}
-			throw new IllegalStateException(
-					"'appServlet' cannot be mapped to '/' under Tomcat versions <= 7.0.14");
-		}
-
-		this.logbackServlet(servletContext);
 	}
 
-	public void logbackServlet(ServletContext servletContext) {
-		ServletRegistration.Dynamic servlet = servletContext.addServlet(
-				"logbackStatus",
-				new ch.qos.logback.classic.ViewStatusMessagesServlet());
-		servlet.setLoadOnStartup(3);
-		Set<String> mappingConflicts = servlet
-				.addMapping("/admin/logbackStatus/*");
-
-		if (!mappingConflicts.isEmpty()) {
-			for (String s : mappingConflicts) {
-				logger.error("Mapping conflict: " + s);
-			}
-			throw new IllegalStateException("servlet cannot be mapped");
-		}
-	}
 
 }
